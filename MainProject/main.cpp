c66018688baaf426e8cf66c6443b35eee43d1456
Matvey Makushenko <matvey.myown@gmail.com>
2025-04-08 18:42:46 +0300

    Test somethint (IDR what specifically)

--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -2,28 +2,34 @@
 
 int main()
 {
-	Student st1, st2;
+	Student* st1 = nullptr;
+	Student* st2 = nullptr;
 
-	st1.name = "Dominik";
-	st1.age = 13;
-	st1.mark = 7.6;
-	st1.alive = true;
+	st1 = new Student;
+	st2 = new Student;
 
-	st2.name = "Ivan";
-	st2.age = 14;
-	st2.mark = 3.4;
-	st2.alive = false;
+	st1->name = "Dominik";
+	st1->age = 13;
+	st1->mark = 7.6;
+	st1->alive = true;
+
+	st2->name = "Ivan";
+	st2->age = 14;
+	st2->mark = 3.4;
+	st2->alive = false;
 
 	cout << "Before: " << endl;
-	cout << st1.getString();
-	cout << st2.getString();
+	cout << st1->getString();
+	cout << st2->getString();
 
 	st1 = st2;
-	st1.name = "Vlad";
+	st1->name = "Vlad";
 
 	cout << "After: " << endl;
-	cout << st1.getString();
-	cout << st2.getString();
+	cout << st1->getString();
+	cout << st2->getString();
+
+	delete st1, st2;
 
 	return 0;
 }
