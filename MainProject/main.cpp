53be6f777d8db0d1d4b67fb05d3fa0784f7b3d9a
Matvey Makushenko <matvey.myown@gmail.com>
2025-04-10 18:43:50 +0300

    Learn copy-constructor

--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -3,14 +3,16 @@
 int main()
 {
 	Student student1;
-	Student student2 ("Alex");
-	Student student3 ("Lesha", 14);
-	Student student4 ("Ivan", 15, 8.4, true);
+	Student student2("Alex");
+	Student student3("Lesha", 14);
+	Student student4("Ivan", 15, 8.4, true);
+	Student student5(student4);
 
 	cout << "Student 1:\n" << student1.toString() << endl;
 	cout << "Student 2:\n" << student2.toString() << endl;
 	cout << "Student 3:\n" << student3.toString() << endl;
 	cout << "Student 4:\n" << student4.toString() << endl;
+	cout << "Student 5: \n" << student5.toString() << endl;
 
 	return 0;
 }
