c4e2def8750374d7cd79dd078dfc7dc6709a792e
Matvey Makushenko <matvey.myown@gmail.com>
2025-05-20 17:54:06 +0300

    Test Stack class with its methods

--- ADSProject/mainStack.cpp
+++ ADSProject/mainStack.cpp
@@ -1,25 +1,25 @@
-#include "Stack.h"
-
-void main(void)
-{
-	Stack stack;
-	int element;
-
-	cout << "Input element into queue (for exit input 0): " << endl;
-	do
-	{
-		cin >> element;
-
-		if (element != 0)
-		{
-			stack.push(element);
-		}
-	} while (element != 0);
-
-	stack.pop();
-	stack.pop();
-	stack.pop();
-
-	cout << stack.toString() << endl;
-
-}
+//#include "Stack.h"
+//
+//void main(void)
+//{
+//	Stack stack;
+//	int element;
+//
+//	cout << "Input element into queue (for exit input 0): " << endl;
+//	do
+//	{
+//		cin >> element;
+//
+//		if (element != 0)
+//		{
+//			stack.push(element);
+//		}
+//	} while (element != 0);
+//
+//	stack.pop();
+//	stack.pop();
+//	stack.pop();
+//
+//	cout << stack.toString() << endl;
+//
+//}
