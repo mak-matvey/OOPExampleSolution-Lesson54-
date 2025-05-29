10b15a4b656681ab9bd23cefdac891d00a0726ba
Matvey Makushenko <matvey.myown@gmail.com>
2025-05-29 18:32:26 +0300

    Update methods Point2D class

--- PointProject/Point2D.cpp
+++ PointProject/Point2D.cpp
@@ -13,25 +13,36 @@ bool Point2D::operator!=(Point2D point)
 
 bool Point2D::operator>(Point2D point)
 {
-	return getLenght() > point.getLenght;
+	return getLenght() > point.getLenght();
 }
 
 bool Point2D::operator<(Point2D point)
 {
-	return getLenght() < point.getLenght;
+	return getLenght() < point.getLenght();
 }
 
 bool Point2D::operator>=(Point2D point)
 {
-	getLenght() >= point.getLenght;
+	return !(*this < point);
 }
 
 bool Point2D::operator<=(Point2D point)
 {
-	getLenght() <= point.getLenght;
+	return !(*this > point);
 }
+
+
 
 double Point2D::getLenght()
 {
 	return sqrt(x * x + y * y);
+}
+
+
+string Point2D::toString()
+{
+	string s = "X = ";
+	s += x + ", Y = ", + y;
+
+	return s;
 }
---
+++ PointProject/main.cpp
@@ -0,0 +1,11 @@
+#include "Point2D.h"
+#include <iostream>
+
+int main()
+{
+	Point2D point1(1, 1), point2(2, 2);
+
+	cout << point1.toString() << point2.toString();
+
+	cout << (point1 > point2) ? "Yes" : "No";
+}
